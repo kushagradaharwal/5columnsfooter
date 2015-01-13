d5588f83b016a599230a5aee07a3f70bb71e64d4
kushagra <kushagra.daharwal@hotmail.com>
2015-01-13 12:33:18 +0530

    get.php

--- get.php
+++ get.php
@@ -23,6 +23,7 @@
  * @package    Mage
  * @copyright  Copyright (c) 2008 Irubin Consulting Inc. DBA Varien (http://www.varien.com)
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * kush check
  */
 if (version_compare(phpversion(), '5.2.0', '<')===true) {
     echo  '<div style="font:12px/1.35em arial, helvetica, sans-serif;"><div style="margin:0 0 25px 0; '
